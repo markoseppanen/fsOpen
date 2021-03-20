9dd846c3b5372d065348c613576c9b66af1e312e
Marko Seppänen <marko.p.seppanen@gmail.com>
2021-03-20 11:26:43 +0200

    courseinfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,30 +1,5 @@
 import React from "react";
 
-const Header = ({ course }) => <h1>{course.name}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ course }) => (
-  <>
-    <Part part={course.parts[0]} />
-    <Part part={course.parts[1]} />
-    <Part part={course.parts[2]} />
-  </>
-);
-
-const Total = ({ course }) => (
-  <p>
-    Number of exercises{" "}
-    {course.parts[0].exercises +
-      course.parts[1].exercises +
-      course.parts[2].exercises}
-  </p>
-);
-
 const App = () => {
   const course = {
     name: "Half Stack application development",
@@ -52,5 +27,30 @@ const App = () => {
     </div>
   );
 };
+
+const Header = ({ course }) => <h1>{course.name}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ course }) => (
+  <>
+    <Part part={course.parts[0]} />
+    <Part part={course.parts[1]} />
+    <Part part={course.parts[2]} />
+  </>
+);
+
+const Total = ({ course }) => (
+  <p>
+    Number of exercises{" "}
+    {course.parts[0].exercises +
+      course.parts[1].exercises +
+      course.parts[2].exercises}
+  </p>
+);
 
 export default App;
