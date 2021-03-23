91a7a17ffda4df409212157939e62700a4ebe19f
Marko Seppänen <marko.p.seppanen@gmail.com>
2021-03-23 15:29:36 +0200

    unicafe: step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -12,22 +11,28 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>statistics</h2>
-      <p>
-        Good: {good}
-        <br />
-        Neutral: {neutral}
-        <br />
-        Bad: {bad}
-        <br />
-        Total: {good + neutral + bad}
-        <br />
-        Average: {(good * 1 + bad * -1) / (good + neutral + bad)}
-        <br />
-        Positive: {(good / (good + neutral + bad)) * 100}%
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
+
+const Statistics = ({ good, neutral, bad }) => (
+  <>
+    <h2>statistics</h2>
+    <p>
+      Good: {good}
+      <br />
+      Neutral: {neutral}
+      <br />
+      Bad: {bad}
+      <br />
+      Total: {good + neutral + bad}
+      <br />
+      Average: {(good * 1 + bad * -1) / (good + neutral + bad)}
+      <br />
+      Positive: {(good / (good + neutral + bad)) * 100}%
+    </p>
+  </>
+);
 
 export default App;
