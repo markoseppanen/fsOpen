17109f98c2935fc1239f2d65a6fd56bb88a144bf
Marko Seppänen <marko@marko.xyz>
2021-10-11 14:28:07 +0300

    unicafe: step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,38 +7,41 @@ const App = () => {
 
   return (
     <div>
-      <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>Give feedback</h2>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
 
-const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <>
-      <h2>statistics</h2>
-      {good + neutral + bad == 0 ? (
-        <p>No Feedback given</p>
-      ) : (
-        <p>
-          Good: {good}
-          <br />
-          Neutral: {neutral}
-          <br />
-          Bad: {bad}
-          <br />
-          Total: {good + neutral + bad}
-          <br />
-          Average: {(good * 1 + bad * -1) / (good + neutral + bad)}
-          <br />
-          Positive: {(good / (good + neutral + bad)) * 100}%
-        </p>
-      )}
-    </>
-  );
-};
+const Statistics = ({ good, neutral, bad }) => (
+  <>
+    <h2>statistics</h2>
+    {good + neutral + bad === 0 ? (
+      <p>No Feedback given</p>
+    ) : (
+      <p>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <StatisticLine text="Total" value={good + neutral + bad} />
+        <StatisticLine text="Average" value={((good * 1 + bad * -1) / (good + neutral + bad)).toFixed(2)}/>
+        <StatisticLine text="Positive" value={`${((good / (good + neutral + bad)) * 100).toFixed(2)}%`}/>
+      </p>
+    )}
+  </>
+);
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticLine = ({ text, value }) => (
+  <p>
+    {text}: {value}
+  </p>
+);
 
 export default App;
