0523a87a429b15ae1d66be968c5f5364dc19cd02
Marko Seppänen <marko@marko.xyz>
2021-11-25 15:26:21 +0200

    phonebook: step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("Name to be added");
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Maija Mallikas", number: "040-11235813" },
+  ]);
+  const [newName, setNewName] = useState("Mikko Mallikas");
+  const [newNumber, setNewNumber] = useState("050-1234567");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -10,13 +13,24 @@ const App = () => {
     if (persons.some((person) => person.name === newName)) {
       window.alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName, id: persons.length + 1 }));
+      setPersons(
+        persons.concat({
+          id: persons.length + 1,
+          name: newName,
+          number: newNumber,
+        })
+      );
       setNewName("");
+      setNewNumber("");
     }
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -27,13 +41,24 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+            type="tel"
+            pattern="[0-9\-]+"
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.id}>{person.name}</li>
+          <li key={person.id}>
+            {person.name} - {person.number}
+          </li>
         ))}
       </ul>
     </div>
