258953a244b706c65004f6e60705780ac7a83351
Marko Seppänen <marko@marko.xyz>
2022-12-02 21:16:14 +0200

    phonebook-backend: step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,29 +1,6 @@
 const express = require('express');
 const app = express();
 
-// const persons = [
-//   {
-//     id: 1,
-//     name: 'Arto Hellas',
-//     number: '040-123456',
-//   },
-//   {
-//     id: 2,
-//     name: 'Ada Lovelace',
-//     number: '39-44-5323523',
-//   },
-//   {
-//     id: 3,
-//     name: 'Dan Abramov',
-//     number: '12-43-234345',
-//   },
-//   {
-//     id: 4,
-//     name: 'Mary Poppendieck',
-//     number: '39-23-6423122',
-//   },
-// ];
-
 let persons = [
   {
     id: 1,
@@ -47,6 +24,8 @@ let persons = [
   },
 ];
 
+app.use(express.json());
+
 app.get('/api/persons', (_request, response) => {
   response.json(persons);
 });
@@ -60,17 +39,27 @@ app.get('/api/persons/:id', (request, response) => {
   response.status(404).end();
 });
 
+app.get('/info', (_request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people.<br/>${Date()}</p>`
+  );
+});
+
+app.post('/api/persons', (request, response) => {
+  console.log('Request Body', request.body);
+  const newPerson = {
+    id: Math.floor(Math.random() * 10000),
+    ...request.body,
+  };
+  console.log('New Person', newPerson);
+  persons = [...persons, newPerson];
+});
+
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter(person => person.id !== id);
 
   response.status(204).end();
-});
-
-app.get('/info', (_request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people.<br/>${Date()}</p>`
-  );
 });
 
 const PORT = 3001;
