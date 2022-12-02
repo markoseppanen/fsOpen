697a12c6c749ef795a4173031f55a1bc3f35aada
Marko Seppänen <marko@marko.xyz>
2022-12-02 17:20:48 +0200

    phonebook-backend: step3+4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+// const persons = [
+//   {
+//     id: 1,
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//   },
+//   {
+//     id: 3,
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//   },
+//   {
+//     id: 4,
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//   },
+// ];
+
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -26,6 +49,22 @@ const persons = [
 
 app.get('/api/persons', (_request, response) => {
   response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  }
+  response.status(404).end();
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
 });
 
 app.get('/info', (_request, response) => {
