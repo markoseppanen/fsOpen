d40af3e040e6a77cf1e41d338ce9905b25cc3c57
Marko Seppänen <marko@marko.xyz>
2023-02-16 09:54:37 +0200

    4.3 Tests, Step1

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,17 +5,23 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
 
 mongoose.set('strictQuery', false) // To suppress warning warning about upcoming change.
-mongoose.connect(mongoUrl)
-
-logger.info('MongoDB Connected')
+mongoose
+  .connect(mongoUrl)
+  .then(() => logger.info('MongoDB Connected'))
+  .catch(error => logger.error('error connecting to MongoDB:', error.message))
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
