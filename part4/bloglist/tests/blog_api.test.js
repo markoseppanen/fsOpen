e90c2b0a60483b269ce8b01bc05df37acf966e8a
Marko Seppänen <marko@marko.xyz>
2023-04-12 15:23:26 +0300

    4.8 Blog list tests, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -10,22 +10,25 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    _id: '63da1e3328967a8901b29f3b',
-    title: 'SchizoBlog',
-    author: 'Janne Toivoniemi',
-    url: 'https://schizoblog.me/',
-    likes: 69,
-    __v: 0
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
   },
   {
-    _id: '63ecc789748aa081c373bd87',
-    title: 'Also Sprach Jussi',
-    author: 'Jussi Mäntysaari',
-    url: 'http://alsosprachjussi.blogspot.com/',
-    likes: 666,
-    __v: 0
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
   }
 ]
+
+const additionalBlog = {
+  title: 'Canonical string reduction',
+  author: 'Edsger W. Dijkstra',
+  url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+  likes: 12
+}
 
 beforeAll(async () => {
   await mongoose.connect(mongoUrl) // Fixes "Jest did not exit"-warning
@@ -43,7 +46,7 @@ afterAll(async () => {
   await mongoose.connection.close()
 })
 
-describe('blog api tests', () => {
+describe('blog api GET-tests', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -65,5 +68,18 @@ describe('blog api tests', () => {
     blogs.forEach(blog => {
       expect(blog.id).toBeDefined()
     })
+  })
+})
+
+describe('blog api POST-tests', () => {
+  test('number of blogs is incremented and data matches', async () => {
+    const newBlog = new Blog(additionalBlog)
+    await newBlog.save()
+
+    const response = await api.get('/api/blogs')
+    const blogs = response.body.map(({ id, ...rest }) => rest)
+
+    expect(blogs).toHaveLength(3)
+    expect(blogs[2]).toEqual(additionalBlog)
   })
 })
