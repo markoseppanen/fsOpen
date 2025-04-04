7faa475f670f2e932e16356c4352be1a3534c245
Marko Seppänen <marko@marko.xyz>
2025-04-04 09:59:02 +0300

    exercises 2.16 + 2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,15 @@ import { Filter } from "./components/Filter";
 import { InputForm } from "./components/InputForm";
 import { PersonList } from "./components/PersonList";
 import * as personService from "./services/persons";
+import { Notification } from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [notificationMessage, setNotificationMessage] = useState(null);
+  const [notificationType, setNotificationType] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => setPersons(initialPersons));
@@ -48,21 +51,45 @@ const App = () => {
         )
       ) {
         const personId = persons.find((person) => person.name === newName).id;
-        personService.update(personId, newPerson).then((updatedPerson) => {
-          setPersons(
-            persons.map((person) =>
-              person.id !== personId ? person : updatedPerson,
-            ),
-          );
-          setNewName("");
-          setNewNumber("");
-        });
+        personService
+          .update(personId, newPerson)
+          .then((updatedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== personId ? person : updatedPerson,
+              ),
+            );
+            setNewName("");
+            setNewNumber("");
+            setNotificationMessage(`Updated ${updatedPerson.name}`);
+            setNotificationType("success");
+            setTimeout(() => {
+              setNotificationMessage(null);
+              setNotificationType(null);
+            }, 5000);
+          })
+          .catch((_error) => {
+            setNotificationMessage(
+              `${newName} was already removed from the server.`,
+            );
+            setNotificationType("error");
+            setTimeout(() => {
+              setNotificationMessage(null);
+              setNotificationType(null);
+            }, 5000);
+          });
       }
     } else {
       personService.add(newPerson).then((person) => {
         setPersons(persons.concat(person));
         setNewName("");
         setNewNumber("");
+        setNotificationMessage(`Added ${person.name}`);
+        setNotificationType("success");
+        setTimeout(() => {
+          setNotificationMessage(null);
+          setNotificationType(null);
+        }, 5000);
       });
     }
   };
@@ -78,6 +105,9 @@ const App = () => {
 
   return (
     <div>
+      {notificationMessage && (
+        <Notification message={notificationMessage} type={notificationType} />
+      )}
       <h1>Phonebook</h1>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <InputForm
